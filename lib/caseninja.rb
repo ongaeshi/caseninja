9aaab20459feb7b0bf7ac94ed988ae04703cd254
ongaeshi <ongaeshi0621@gmail.com>
2015-08-29 15:04:27 +0900

    Add Caseninja#exec(text)

--- lib/caseninja.rb
+++ lib/caseninja.rb
@@ -1,5 +1,18 @@
 require "caseninja/version"
 
 module Caseninja
-  # Your code goes here...
+  module_function
+
+  def exec(text, options = {})
+    "Caseninja converts input text to chain case"
+
+    {
+      chain: "caseninja-converts-input-text-to-chain-case",
+      snake: "caseninja_converts_input_text_to_chain_case",
+      camel: "caseninjaConvertsInputTextToChainCase",
+      pascal: "CaseninjaConvertsInputTextToChainCase",
+      upper_chain: "CASENINJA-CONVERTS-INPUT-TEXT-TO-CHAIN-CASE",
+      upper_snake: "CASENINJA_CONVERTS_INPUT_TEXT_TO_CHAIN_CASE",
+    }
+  end
 end
