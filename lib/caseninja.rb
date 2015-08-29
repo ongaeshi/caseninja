e74eb46ff4e4d4d3e6287f16b638d9326e32f137
ongaeshi <ongaeshi0621@gmail.com>
2015-08-29 15:36:40 +0900

    Implement upper_chain, upper_snake

--- lib/caseninja.rb
+++ lib/caseninja.rb
@@ -9,8 +9,8 @@ module Caseninja
       snake: to_snake(text),
       camel: to_camel(text),
       pascal: to_pascal(text),
-      upper_chain: "HELLO-WORLD",
-      upper_snake: "HELLO_WORLD",
+      upper_chain: to_upper_chain(text),
+      upper_snake: to_upper_snake(text),
     }
   end
 
@@ -29,4 +29,13 @@ module Caseninja
   def to_pascal(text)
     text.split.map { |e| e.capitalize }.join
   end
+
+  def to_upper_chain(text)
+    to_chain(text).upcase
+  end
+  
+  def to_upper_snake(text)
+    to_snake(text).upcase
+  end
+  
 end
